a26d0f44cff1c5a4fb772fc64fa3e1119cb70a7a
srackley <shelbyrackleyart@gmail.com>
2017-10-25 17:51:49 -0400

    Home page styled

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,13 @@ import { Jumbotron, Button } from 'react-bootstrap';
 const Home = () => (
   <div id="home">
     <Jumbotron>
-      <div id="home-text">
+      <div id="home-text" className="shadow4 center">
         <h1>Hello, world!</h1>
         <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-        <p><Button bsStyle="success" bsSize="large" block>Learn more</Button></p>
+        <p><Button bsStyle="success" bsSize="large" className="shadow1">Learn more</Button></p>
       </div>
     </Jumbotron>
+    <div />
   </div>
 );
 
