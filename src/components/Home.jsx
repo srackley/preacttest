022f9bb0a2a7a484da4b06023e433f98e13e174b
srackley <shelbyrackleyart@gmail.com>
2018-01-01 19:39:08 -0600

    Fix safari display bugs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Typed from 'typed.js';
+import { NavLink } from 'react-router-dom';
 
 export default class Home extends React.Component {
   componentDidMount() {
     const strings = [
-      'a creator.', 'passionate.', 'a designer.', 'efficient.', 'a communicator.', 'helpful.', 'a leader.', 'a teacher.', 'curious.', 'a maker.', 'creative.', 'dedicated.', 'intelligent.', 'an explorer.', 'enthusiastic.', 'an artist.', 'versatile.', 'resourceful.', 'clever.', 'productive.', 'skillful.', 'an innovator.', 'confident.', 'a fast learner.', 'focused.', 'logical.', 'persistent.', 'honest.', 'disciplined.', 'humble.',
+      'a creator.', 'passionate.', 'a designer.', 'efficient.', 'current.', 'helpful.', 'a leader.', 'a teacher.', 'curious.', 'a maker.', 'creative.', 'dedicated.', 'intelligent.', 'an explorer.', 'enthusiastic.', 'an artist.', 'versatile.', 'resourceful.', 'clever.', 'productive.', 'skillful.', 'an innovator.', 'confident.', 'a fast learner.', 'focused.', 'logical.', 'persistent.', 'honest.', 'disciplined.', 'humble.',
     ];
     const options = {
       strings,
@@ -27,19 +28,20 @@ export default class Home extends React.Component {
           <h1>HELLO!</h1>
           <h2>My name is Shelby Rackley. <br /> I am a Full Stack Web Developer. <br /> I am also <span id="typed" ref={(el) => { this.el = el; }} />
           </h2>
-          <a href="/about">
+
+        </div>
+        <div className="bottom">
+          <NavLink to="/about" >
             <button id="heroButton" className="shadow1 outline-button animate fadeInUp three">About</button>
+          </NavLink>
+          <a
+            href="/ShelbyRackley.pdf"
+            download
+            id="resumeLink"
+            className="animate fadeIn eight"
+          >
+            <h3>I am currently seeking full-time opportunities!</h3>
           </a>
-          <div>
-            <a
-              href="/ShelbyRackley.pdf"
-              download
-              id="resumeLink"
-              className="animate fadeIn eight"
-            >
-              <h3>I am currently seeking full-time opportunities!</h3>
-            </a>
-          </div>
         </div>
       </div>
     );
