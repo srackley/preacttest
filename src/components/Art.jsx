ff91080ba8b33b7dbacbf29c60b560df0a14c84c
srackley <shelbytrackley@gmail.com>
2018-02-05 16:28:25 -0500

    Style console messages

--- src/components/Art.jsx
+++ src/components/Art.jsx
@@ -186,7 +186,7 @@ const PHOTO_SET = [
 
 const Art = () => (
   <div>
-    {console.log('Yay! You found my artwork. Bathe in the beauty of my work.')}
+    {console.log('%c Yay! You found my artwork. Bathe in the beauty of my work.', 'font-family: Verdana; line-height: 32px; font-size:20px;color:#fff;text-shadow:0 1px 0#ccc,0 2px 0  #c9c9c9 ,0 3px 0  #bbb ,0 4px 0  #b9b9b9 ,0 5px 0  #aaa ,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);')}
     <div style={{ height: '100px' }} />
     <Gallery photos={PHOTO_SET} />
   </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = ({ title }) => (
   <div className="header">
-    <h1 className="animate fadeInLeft one">{title}</h1>{console.log('Congratulations! You found me. For an extra artistic bonus, click on my nose on the About page.')}
+    <h1 className="animate fadeInLeft one">{title}</h1>{console.log('%c Congratulations! \n You found me. For an extra artistic bonus, click on my nose on the About page.', 'font-family: Verdana; line-height: 32px; font-size:20px;color:#fff;text-shadow:0 1px 0#ccc,0 2px 0  #c9c9c9 ,0 3px 0  #bbb ,0 4px 0  #b9b9b9 ,0 5px 0  #aaa ,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);')}
     <span id="bars">
       <div className="bar light neonBars animate one super" style={{ width: '10px' }} />
       <div className="bar light neonBars animate one super" style={{ width: '40px' }} />
